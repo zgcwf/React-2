ff6210a01c7c3054a8b18d4017b5a84e8ac724ed
zgcwf <1935083614@qq.com>
2021-11-02 20:09:11 +0800

    源码+笔记

--- react_staging/01_脚手架自带文件/src/index.js
+++ react_staging/01_脚手架自带文件/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+//  <React.StrictMode>用于校验一些错误，如：字符串形式的ref(将要被弃用，不再推荐写)
 ReactDOM.render(
   <React.StrictMode>
     <App />
--- react_staging/02_src_hello_react/App.jsx
+++ react_staging/02_src_hello_react/App.jsx
@@ -1,15 +1,16 @@
 //创建“外壳”组件App
-import React,{Component} from 'react'
+import React, { Component } from 'react'
+// 得到Component这里是分别暴露，不是解构赋值
 import Hello from './components/Hello'
 import Welcome from './components/Welcome'
 
 //创建并暴露App组件
-export default class App extends Component{
-	render(){
+export default class App extends Component {
+	render() {
 		return (
 			<div>
-				<Hello/>
-				<Welcome/>
+				<Hello />
+				<Welcome />
 			</div>
 		)
 	}
