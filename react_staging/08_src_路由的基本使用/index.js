83d32cf3f4ea2c4efa6473c7f4f002a8fa3d38df
zgcwf <1935083614@qq.com>
2021-11-05 15:10:57 +0800

    源码+笔记

--- react_staging/08_src_路由的基本使用/index.js
+++ react_staging/08_src_路由的基本使用/index.js
@@ -2,14 +2,14 @@
 import React from 'react'
 //引入ReactDOM
 import ReactDOM from 'react-dom'
-//
-import {BrowserRouter} from 'react-router-dom'
+// 引入BrowserRouter，包裹住App
+import { BrowserRouter } from 'react-router-dom'
 //引入App
 import App from './App'
 
 ReactDOM.render(
 	<BrowserRouter>
-		<App/>
+		<App />
 	</BrowserRouter>,
 	document.getElementById('root')
 )
